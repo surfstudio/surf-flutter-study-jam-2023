24d8722f6a22ddc66cac6fe5e78800c17d4a61ce
DenisTc <TsapaDe@yandex.ru>
2023-04-09 15:40:21 +0600

    Добавил класс AppColors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// Класс AppColors содержит константы цветов, используемых в приложении.
+abstract class AppColors {
+  /// Цвет фона главного окна приложения.
+  static const scaffoldBackgroundColor = Color(0xFFfffbfe);
+
+  static const black = Colors.black;
+  static const lavender = Color(0xFFeaddff);
+  static const royalPurple = Color(0xFF6750a4);
+}
