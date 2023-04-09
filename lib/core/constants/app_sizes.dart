dc64b3554f9846c448f92a96be090dc8eb76927d
DenisTc <TsapaDe@yandex.ru>
2023-04-09 15:38:16 +0600

    Класс для хранения константных размеров

---
+++ lib/core/constants/app_sizes.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+/// Класс для хранения константных размеров (paddings, gaps, rounded corners...)
+abstract class AppSizes {
+  static const p0 = 0.0;
+  static const p2 = 2.0;
+  static const p4 = 4.0;
+  static const p8 = 8.0;
+  static const p12 = 12.0;
+  static const p14 = 14.0;
+  static const p16 = 16.0;
+  static const p24 = 24.0;
+  static const p32 = 32.0;
+  static const p40 = 40.0;
+  static const p50 = 50.0;
+
+  static const gapH40 = SizedBox(height: AppSizes.p40);
+
+  static const borderRadius16 = BorderRadius.all(Radius.circular(AppSizes.p16));
+
+  static const roundedRectangleBorder16 = RoundedRectangleBorder(
+    borderRadius: borderRadius16,
+  );
+
+  static const edgeSymmetricV16H32 =
+      EdgeInsets.symmetric(vertical: AppSizes.p16, horizontal: AppSizes.p32);
+}
