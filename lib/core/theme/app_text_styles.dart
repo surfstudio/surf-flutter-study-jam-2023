27e294bed8cbef608e7e0bc80725bfc586490854
DenisTc <TsapaDe@yandex.ru>
2023-04-09 15:41:46 +0600

    Добавил класс с кастомными стилями для текста AppTextStyle

---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:surf_flutter_study_jam_2023/core/constants/app_colors.dart';
+import 'package:surf_flutter_study_jam_2023/core/constants/app_sizes.dart';
+
+/// Класс AppTextStyle содержит константы текстовых стилей, используемых в приложении.
+class AppTextStyle {
+  static const bodyText = TextStyle(
+    color: AppColors.black,
+    fontSize: AppSizes.p16,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.41,
+  );
+
+  static const buttonText = TextStyle(
+    color: AppColors.black,
+    fontSize: AppSizes.p14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.41,
+  );
+}
