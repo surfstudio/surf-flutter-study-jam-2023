b9ef98606a3f5845982e10dd7ac63f98bee1bb14
DenisTc <TsapaDe@yandex.ru>
2023-04-09 16:00:18 +0600

    Подключил виджеты на главнй экран

--- lib/features/ticket_storage/ticket_storage_page.dart
+++ lib/features/ticket_storage/ticket_storage_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:surf_flutter_study_jam_2023/core/theme/app_text_styles.dart';
+import 'package:surf_flutter_study_jam_2023/features/ticket_storage/widgets/add_ticket_button_widget.dart';
 
 /// Экран “Хранения билетов”.
 class TicketStoragePage extends StatelessWidget {
@@ -6,6 +9,22 @@ class TicketStoragePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const FlutterLogo();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          AppLocalizations.of(context)!.ticketStorage,
+          style: const TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      floatingActionButton: const AddTicketButtonWidget(),
+      body: Center(
+        child: Text(
+          AppLocalizations.of(context)!.noAvailableData,
+          style: AppTextStyle.bodyText,
+        ),
+      ),
+    );
   }
 }
